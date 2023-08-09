44b0654aeb3fcd6751f6a42f97d61073f025ad04
miguel4521 <109853127+miguel4521@users.noreply.github.com>
2023-08-09 11:10:24 +0100

    first commit

---
+++ src/NavBar.js
@@ -0,0 +1,13 @@
+const NavBar = () => {
+    return (
+        <div>
+            <a href="#home">Home</a>
+            <a href="#research">Research</a>
+            <a href="#people">People</a>
+            <a href="publications">Publications</a>
+            <a href="Contact">Contact</a>
+        </div>
+    )
+}
+
+export default NavBar;
