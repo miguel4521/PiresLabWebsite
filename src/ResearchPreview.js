9e374e2bf65bf56f0793de13a1c85302a45c54ab
miguel4521 <109853127+miguel4521@users.noreply.github.com>
2023-08-15 18:32:07 +0100

    improved images and text on home screen

--- src/ResearchPreview.js
+++ src/ResearchPreview.js
@@ -5,8 +5,8 @@ const ResearchPreview = ({researchTitle, imageSrc, id}) => {
     return (
         <div className="page-container">
             <NavLink to={`/research#${id}`} className="content-container research-preview">
-                <img src={imageSrc} className="research-img" alt="" />
                 <div className="research-title">{researchTitle}</div>
+                <img src={imageSrc} className="research-img" alt="" />
                 <div className="foot-txt">Learn more</div>
             </NavLink>
         </div>
